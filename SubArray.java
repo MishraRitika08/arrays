e47c0aed7a39f10447028c272bb05e316685c4f0
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-24 01:54:13 +0530

    Kadan's algo

--- SubArray.java
+++ SubArray.java
@@ -1,3 +1,25 @@
 public class SubArray {
+
+  public static int kadansAlgo(int arr[]) {
+
+    int ms=Integer.MIN_VALUE;
+    int cs=0;
+
+    for(int i=0;i<arr.length;i++){
+        cs=cs+arr[i];
+
+        if(cs<0){
+            cs=0;
+        }
+
+        ms=Math.max(cs,ms);
+    }
+    return ms;
     
+    }
+  
+    public static void main(String[] args) {
+        int height[]={1,3,4,7};
+      System.out.println(kadansAlgo(height));
+    }
 }
--- reverse.java
+++ reverse.java
@@ -3,7 +3,7 @@ public class reverse {
     public static void reverseArr(int arr[]){
         int si=0;
         int li=arr.length-1;
-        while(si<=li){
+        while(si<li){
 
             int temp=arr[si];
             arr[si]=arr[li];
@@ -15,7 +15,7 @@ public class reverse {
     }
 
     public static void main(String[] args) {
-     int height[]={4,2,6,2,7};
+     int height[]={4,3,2,7};
      reverseArr(height);
      for(int i=0;i<height.length;i++){
         System.out.print(height[i]+" ");
