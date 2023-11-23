e729612c559254f1f62b1fc2ac8090180092cd3e
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-23 11:27:47 +0530

    buy and sell stocks problem

--- buyAndSellStocks.java
+++ buyAndSellStocks.java
@@ -1,3 +1,25 @@
 public class buyAndSellStocks {
-    
+    public static int buyAndSell(int arr[]) {
+
+        int buyPrice = arr[0];
+        int maxProfit=0;
+
+        for(int i=1;i<arr.length;i++){
+
+            if(buyPrice<arr[i]){
+                int profit = arr[i]-buyPrice;
+                maxProfit=Math.max(maxProfit,profit);
+            }
+            else{
+                buyPrice=arr[i];
+            }
+        }
+        
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int price[]={7,2,6,1,5};
+        System.out.print(buyAndSell(price));
+    }
 }
