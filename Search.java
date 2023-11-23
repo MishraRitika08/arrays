b70cf8073080f0817369981c61b993c6bf387c19
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-23 11:43:16 +0530

    Binary Search

--- Search.java
+++ Search.java
@@ -1,3 +1,27 @@
 public class Search {
-    
+    public static int binarySearch(int arr[],int key){
+        int si=0;
+        int ei=arr.length-1;
+
+        while(si<ei){
+
+            int mid=si+(ei-si)/2;
+
+            if(arr[mid]==key){
+              return mid;
+            }
+            else if(arr[mid]<key){
+                si=mid+1;
+            }
+            else{
+                ei=mid-1;
+            }
+        }
+        return-1;   
+    }
+
+    public static void main(String[] args) {
+        int height[]={1,3,4,7};
+        System.out.println(binarySearch(height,7));
+    }
 }
