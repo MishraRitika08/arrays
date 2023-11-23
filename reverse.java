937c6515bcdfe1865cbe78299b6a546611e13cf7
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-24 00:49:48 +0530

    Array reverse

--- reverse.java
+++ reverse.java
@@ -1,3 +1,24 @@
 public class reverse {
     
+    public static void reverseArr(int arr[]){
+        int si=0;
+        int li=arr.length-1;
+        while(si<=li){
+
+            int temp=arr[si];
+            arr[si]=arr[li];
+            arr[li]=temp;
+            
+            li--;
+            si++;
+        }
+    }
+
+    public static void main(String[] args) {
+     int height[]={4,2,6,2,7};
+     reverseArr(height);
+     for(int i=0;i<height.length;i++){
+        System.out.print(height[i]+" ");
+     }
+    }
 }
